3c779fed8ae11ad0324d941e7caa8d9c4abb95a7
WilliamJolivet <80533655+Pas2Pseudo@users.noreply.github.com>
2022-12-26 13:31:26 +0100

    Create SimpleTaskImpl.java

---
+++ Impl/src/main/java/fr/hyriode/cosmetics/task/SimpleTaskImpl.java
@@ -0,0 +1,47 @@
+package fr.hyriode.cosmetics.task;
+
+import fr.hyriode.cosmetics.HyriCosmeticsPlugin;
+import org.bukkit.scheduler.BukkitRunnable;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+public class SimpleTaskImpl extends BukkitRunnable implements SimpleTask {
+
+    private final TaskProvider provider;
+    private final List<UUID> tasksUUID;
+
+    public SimpleTaskImpl(TaskProvider provider) {
+        this.provider = provider;
+        this.tasksUUID = new ArrayList<>();
+    }
+
+    @Override
+    public void init() {
+        this.runTaskTimer(HyriCosmeticsPlugin.getPlugin(HyriCosmeticsPlugin.class), 0, 1L);
+    }
+
+    @Override
+    public void stop() {
+        this.cancel();
+        provider.getNodes().remove(this);
+    }
+
+    @Override
+    public void run() {
+        getCosmeticTasks().forEach(CosmeticTask::run);
+    }
+
+    @Override
+    public List<CosmeticTask> getCosmeticTasks() {
+        List<CosmeticTask> result = new ArrayList<>();
+        tasksUUID.forEach(uuid -> result.add(provider.getTask(uuid)));
+        return result;
+    }
+
+    @Override
+    public List<UUID> getTasksUUID() {
+        return tasksUUID;
+    }
+}
