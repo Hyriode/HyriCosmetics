29ff7ee2cbbfc9b168c639a62e2ffd2cb2e232df
WilliamJolivet <80533655+Pas2Pseudo@users.noreply.github.com>
2022-12-26 13:33:23 +0100

    Create ParticleManager.java

---
+++ API/src/main/java/fr/hyriode/cosmetics/particle/ParticleManager.java
@@ -0,0 +1,8 @@
+package fr.hyriode.cosmetics.particle;
+
+import fr.hyriode.cosmetics.common.CosmeticManager;
+
+import java.util.List;
+
+public interface ParticleManager extends CosmeticManager<AbstractParticle> {
+}
