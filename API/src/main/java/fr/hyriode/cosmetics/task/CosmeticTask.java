c51380946ffa2f3fdfa28fc73afbcfa22f9856df
WilliamJolivet <80533655+Pas2Pseudo@users.noreply.github.com>
2022-12-26 13:33:16 +0100

    Update CosmeticTask.java

--- API/src/main/java/fr/hyriode/cosmetics/task/CosmeticTask.java
+++ API/src/main/java/fr/hyriode/cosmetics/task/CosmeticTask.java
@@ -1,15 +1,13 @@
 package fr.hyriode.cosmetics.task;
 
-import fr.hyriode.cosmetics.task.node.TaskNode;
-
 import java.util.UUID;
 
 public interface CosmeticTask extends Runnable {
 
     UUID getUUID();
 
-    TaskNode getNode();
+    SimpleTask getNode();
 
-    void assignNode(TaskNode node);
+    void assignNode(SimpleTask node);
 
 }
