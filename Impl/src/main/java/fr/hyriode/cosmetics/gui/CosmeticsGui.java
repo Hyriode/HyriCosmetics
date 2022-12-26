c0c14d0ea9bdfb09665f62ca32c8c31beacb2e15
WilliamJolivet <80533655+Pas2Pseudo@users.noreply.github.com>
2022-12-26 13:32:49 +0100

    Update CosmeticsGui.java

--- Impl/src/main/java/fr/hyriode/cosmetics/gui/CosmeticsGui.java
+++ Impl/src/main/java/fr/hyriode/cosmetics/gui/CosmeticsGui.java
@@ -1,4 +1,15 @@
 package fr.hyriode.cosmetics.gui;
 
-public class CosmeticsGui {
+import fr.hyriode.api.player.IHyriPlayer;
+import fr.hyriode.hyrame.inventory.HyriInventory;
+import org.bukkit.entity.Player;
+
+public class CosmeticsGui extends HyriInventory {
+
+    protected final IHyriPlayer account;
+
+    public CosmeticsGui(Player owner, String name, int size) {
+        super(owner, name, size);
+        this.account = IHyriPlayer.get(owner.getUniqueId());
+    }
 }
