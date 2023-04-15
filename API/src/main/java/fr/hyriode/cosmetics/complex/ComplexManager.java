5413b57f85754b5f62b53c0df6945bcfff4dd65c
Pas2Pseudo <williamjolivet74@gmail.com>
2023-04-15 15:40:49 +0200

    removal of the cosmetic system by mesh in view of a new system

--- API/src/main/java/fr/hyriode/cosmetics/complex/ComplexManager.java
+++
@@ -1,7 +0,0 @@
-package fr.hyriode.cosmetics.complex;
-
-import fr.hyriode.cosmetics.common.CosmeticManager;
-
-public interface ComplexManager extends CosmeticManager<AbstractComplex> {
-
-}
--- API/src/main/java/fr/hyriode/cosmetics/complex/animation/CosmeticAnimation.java
+++
@@ -1,9 +0,0 @@
-package fr.hyriode.cosmetics.complex.animation;
-
-import java.util.List;
-
-public interface CosmeticAnimation {
-
-    List<AnimationPoint> getPoints();
-
-}
