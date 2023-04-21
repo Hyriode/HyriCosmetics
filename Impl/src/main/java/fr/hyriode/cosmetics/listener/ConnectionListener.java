8ae9d795c8952d3b08097078b1ba25328e3f68fb
Pas2Pseudo <williamjolivet74@gmail.com>
2023-04-21 12:33:26 +0200

    Update ConnectionListener.java

--- Impl/src/main/java/fr/hyriode/cosmetics/listener/ConnectionListener.java
+++ Impl/src/main/java/fr/hyriode/cosmetics/listener/ConnectionListener.java
@@ -1,8 +1,5 @@
 package fr.hyriode.cosmetics.listener;
 
-import fr.hyriode.api.HyriAPI;
-import fr.hyriode.api.player.IHyriPlayer;
-import fr.hyriode.api.player.IHyriPlayerSession;
 import fr.hyriode.cosmetics.HyriCosmetics;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
@@ -15,10 +12,6 @@ public class ConnectionListener implements Listener {
     @EventHandler(priority = EventPriority.HIGH)
     public void onPlayerJoin(PlayerJoinEvent event) {
         HyriCosmetics.get().getUserProvider().createUser(event.getPlayer());
-        final IHyriPlayer hyriPlayer = HyriAPI.get().getPlayerManager().getPlayer(event.getPlayer().getUniqueId());
-        hyriPlayer.getHyodes().add(50000).exec();
-        hyriPlayer.getHyris().add(50000).exec();
-        hyriPlayer.update();
     }
 
     @EventHandler(priority = EventPriority.HIGH)
